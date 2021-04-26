3600f57468134a9b2342c637325263c241da1f03
蔡金锋 <jeff.cai@99bill.com>
2021-04-26 13:44:09 +0800

    默认环境配置调整

--- template/config/env.js
+++ template/config/env.js
@@ -5,16 +5,18 @@ module.exports = {
     REACT_APP_API: ""
   },
   dev: {
+    // PUBLIC_URL: "http://test.example.com/path/",
+    // REACT_APP_API: "http://api.example.com",
     REACT_APP_ENV: "dev"
   },
   test: {
-    PUBLIC_URL: "http://test.example.com/path/",
-    REACT_APP_API: "http://api.example.com",
+    // PUBLIC_URL: "http://test.example.com/path/",
+    // REACT_APP_API: "http://api.example.com",
     REACT_APP_ENV: "test"
   },
   prod: {
-    PUBLIC_URL: "http://example.com/path/",
-    REACT_APP_API: "http://api.example.com",
+    PUBLIC_URL: "/", // http://example.com/path/
+    REACT_APP_API: "", // http://api.example.com
     REACT_APP_ENV: "prod",
     GENERATE_SOURCEMAP: "false",
     BUILD_PATH: "dist"
