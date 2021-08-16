cd6060b37664bfaa1f71744da5ef34d61bead775
蔡金锋 <jeff.cai@99bill.com>
2021-08-16 09:25:38 +0800

    环境变量修正

--- template/src/react-app-env.d.ts
+++ template/src/react-app-env.d.ts
@@ -1,5 +1,10 @@
 /// <reference types="react-scripts" />
 
-declare const REACT_APP_API: string;
 declare module '*.less';
-declare const delay: string;
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly REACT_APP_ENV: 'dev' | 'test' | 'prod';
+    readonly REACT_APP_API: string;
+  }
+}
