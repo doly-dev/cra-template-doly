a06c8cf849110799abe31d200769f5c1a696b5b4
caijf <369756941@qq.com>
2023-04-11 10:37:55 +0800

    feat: 本地存储改用cache2

--- template/src/utils/storage.ts
+++ template/src/utils/storage.ts
@@ -1,16 +1,18 @@
-import store from 'store2';
+import Cache2 from 'cache2';
 
 // 本地存储命名空间，建议改为项目名
 const STORE_NAMESPACE = 'cra-template-doly';
-const { session } = store.namespace(STORE_NAMESPACE);
+const cache = new Cache2(STORE_NAMESPACE, {
+  storage: window.sessionStorage
+});
 
 // 登录信息
 const LOGIN_INFO_KEY = 'loginInfo';
 
-export const getLoginInfo = () => session.get(LOGIN_INFO_KEY) as (API.Login['data'] | undefined);
+export const getLoginInfo = () => cache.get(LOGIN_INFO_KEY) as (API.Login['data'] | undefined);
 export const setLoginInfo = (data: API.Login['data']) => {
-  session.set(LOGIN_INFO_KEY, data);
+  cache.set(LOGIN_INFO_KEY, data);
 };
 export const removeLoginInfo = () => {
-  session.remove(LOGIN_INFO_KEY);
+  cache.del(LOGIN_INFO_KEY);
 };
